797bb969e64de32df632eea0ec2e2a20e4dca9d9
Martina Mangione <m.mangione95@gmail.com>
2017-06-18 15:50:54 +0200

    12. Add register link in the navbar

--- Gallery/src/main/java/it/uniroma3/spring/security/SecurityConfig.java
+++ Gallery/src/main/java/it/uniroma3/spring/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     	 http
          .csrf().disable()
          .authorizeRequests()
-         	.antMatchers("/","/showAuthorList","/showAllPictures","/img/**", "/image/**", "/css/**", "/js/**", "/fonts/**").permitAll()
+         	.antMatchers("/","/showAuthorList","/showAllPictures","/register","/img/**", "/image/**", "/css/**", "/js/**", "/fonts/**").permitAll()
              .anyRequest().authenticated()
              .and()
          .formLogin()
