1c306e938981f7094d5ea5f6b2dc44fe5ba0af82
Martina Mangione <m.mangione95@gmail.com>
2017-06-19 01:45:38 +0200

    16. To fix: personalArea + personalArea controller and register button link

--- Gallery/src/main/java/it/uniroma3/spring/controller/PersonalAreaController.java
+++ Gallery/src/main/java/it/uniroma3/spring/controller/PersonalAreaController.java
@@ -1,16 +1,15 @@
 package it.uniroma3.spring.controller;
 
 	import org.springframework.stereotype.Controller;
-	import org.springframework.ui.Model;
 	import org.springframework.web.bind.annotation.RequestMapping;
 
 	// controller to access the personalArea
 	@Controller
 	public class PersonalAreaController {
 
-	  // Login form
+	 
 	  @RequestMapping("/personalArea")
-	  public String login() {
+	  public String personalArea() {
 	    return "personalArea";
 	  }
 
